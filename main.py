ba78966f7f931a9364c60dafa255c0a5494827cc
AshwinBhaskaran42 <ashwinbhaskaran42@gmail.com>
2025-08-16 03:26:30 +0530

    Sixth commit

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI, Form, Request
 from fastapi.responses import PlainTextResponse
 from twilio.rest import Client
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv("keys.env")
+# load_dotenv("keys.env")
 account_sid=os.getenv("account_sid")
 auth_token=os.getenv("auth_token")
 client= Client(account_sid,auth_token)
